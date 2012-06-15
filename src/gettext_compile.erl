ded08c5e2fe87dc97f9e3aafbf1ae9e27a49b6e8
p2k <patrick.p2k.schneider@gmail.com>
2012-06-15 19:16:18 +0200

    Scanner Support for gettext:key2str/2 and gettext:key2str/3
    This small change allows to choose from gettext:key2str(Key), gettext:key2str(Key, Lang) and gettext:key2str(Server, Key, Lang) in your projects. This is achieved by ignoring the other parameters to gettext:key2str in the scanner.

--- src/gettext_compile.erl
+++ src/gettext_compile.erl
@@ -236,18 +236,8 @@ pt([H|T],Opts,Func) when is_list(H) ->
     F = fun (X) -> pt(X,Opts,Func) end,
     [lists:map(F,H)|pt(T,Opts,Func)];
 %%%
-pt({call,L1,{remote,L2,{atom,L3,gettext},{atom,L4,key2str}},
-    [{string,L5,String}]}, _Opts, _Func) ->
-    ?debug( "++++++ String=<~p>~n",[String]),
-    dump(String, L5),
-    {call,L1,
-     {remote,L2,
-      {atom,L3,gettext},
-      {atom,L4,key2str}},
-     [{string,L5,String}]};
-%%%
 pt([{call,_,{remote,_,{atom,_,gettext},{atom,_,key2str}},
-    [{string,L5,String}]} = H | T], Opts, Func) ->
+    [{string,L5,String} | _]} = H | T], Opts, Func) ->
     ?debug( "++++++ String=<~p>~n",[String]),
     dump(String, L5),
     [H | pt(T, Opts, Func)];
